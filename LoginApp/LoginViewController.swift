b35c0e48392a0049e01538de320ec9a444abd2f1
Alexey Barinov <begoodbebad2012@gmail.com>
2021-03-30 19:26:43 +0300

    Added: Code refactoring StoryBoard refactoring

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var loginTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
+    let login = "Admin"
+    let password = "1234"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +25,22 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginButtonTapped() {
-        loginTF.text == "Admin" && passwordTF.text == "1234" ?
+        loginTF.text == login && passwordTF.text == password ?
             performSegue(withIdentifier: "toHelloVC", sender: nil) :
             showAlert(with: "Error", and: "Enter correct Login and Password")
-        
-        loginTF.text = .none
-        passwordTF.text = .none
     }
     
     @IBAction func remindLoginTapped() {
-        showAlert(with: "Your Login", and: "Admin")
+        showAlert(with: "Your Login", and: login)
     }
     
     @IBAction func remindPasswordTapped() {
-        showAlert(with: "Your Password", and: "1234")
+        showAlert(with: "Your Password", and: password)
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard let helloVC = segue.source as? HelloViewController else { return }
-        loginTF.text = helloVC.userName
+        loginTF.text = .none
+        passwordTF.text = .none
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,7 +48,38 @@ class LoginViewController: UIViewController {
         helloVC.userName = loginTF.text
     }
     
+    // MARK: Скрыть клавиатуру
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super .touchesBegan(touches, with: event)
+    view.endEditing(true)
+    }
+    
+    // MARK: Кнопка Next & Done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    let nextTFTag = textField.tag + 1
 
+    if let nextResponder = textField.superview?.viewWithTag(nextTFTag) {
+        nextResponder.becomeFirstResponder()
+    } else {
+        loginButtonTapped()
+    }
+         return true
+    }
+    
+    // MARK: Алерт контроллер
+     private func showAlert(with title: String, and message: String) {
+     let alert = UIAlertController(title: title,
+                                   message: message,
+                                   preferredStyle: .alert)
+        
+     let action = UIAlertAction(title: "Ok", style: .cancel) { _ in
+        self.loginTF.text = .none
+        self.passwordTF.text = .none
+     }
+     
+     alert.addAction(action)
+     present(alert, animated: true)
+     }
 }
 
 
